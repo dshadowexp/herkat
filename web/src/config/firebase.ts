750d08bf47d5e740690037ed158f5f1acc481efc
Annonysam <annonysam@Awuahs-MacBook-Air.local>
2024-06-25 01:44:26 -0400

    Removed the firebase secrets

--- web/src/config/firebase.ts
+++ web/src/config/firebase.ts
@@ -3,13 +3,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAsXn8rNSWatdEquG98MUX5g8aKqiVlfpE",
-  authDomain: "herkat-3290c.firebaseapp.com",
-  projectId: "herkat-3290c",
-  storageBucket: "herkat-3290c.appspot.com",
-  messagingSenderId: "341678091521",
-  appId: "1:341678091521:web:419f87571e0480d456ff00",
-  measurementId: "G-GBNMXXCCC7"
+
 };
 
 // Initialize Firebase
